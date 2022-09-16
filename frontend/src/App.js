616b4a78aa5058bc80313a0b49babfad7cf6c70c
Adefila Mustapha <adeope63@gmail.com>
2022-09-16 19:40:59 +0100

    First Section

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href='/'>Amazon</a>
+      </header>
+      <main>
+        list Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
