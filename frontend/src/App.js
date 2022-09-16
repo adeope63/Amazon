0eb2de4f42e9a2e4cf3ff7afdf110d2e315ea25b
Adefila Mustapha <adeope63@gmail.com>
2022-09-16 21:45:34 +0100

    Stage 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
-
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <a href='/'>Amazon</a>
+        <a href="/">Amazon</a>
       </header>
       <main>
-        list Products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="productInfo">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to Cart</button>
+                
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
